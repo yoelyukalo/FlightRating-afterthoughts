e580cb9d8c62057e2973a814e61f18c0754010ca
yoelyukalo <yoelyukalo@gmail.com>
2021-04-08 03:27:27 -0500

    scaffold Airline

---
+++ test/system/airlines_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class AirlinesTest < ApplicationSystemTestCase
+  setup do
+    @airline = airlines(:one)
+  end
+
+  test "visiting the index" do
+    visit airlines_url
+    assert_selector "h1", text: "Airlines"
+  end
+
+  test "creating a Airline" do
+    visit airlines_url
+    click_on "New Airline"
+
+    click_on "Create Airline"
+
+    assert_text "Airline was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Airline" do
+    visit airlines_url
+    click_on "Edit", match: :first
+
+    click_on "Update Airline"
+
+    assert_text "Airline was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Airline" do
+    visit airlines_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Airline was successfully destroyed"
+  end
+end
